d930b3a1f47fa3db06d66dc784915bebeeb8bfbc
sam <sam@silver42.karoo.co.uk>
2017-07-10 20:43:01 +0100

    Get rect rendering working

--- src/renderer/ThreeJsRenderer.hx
+++ src/renderer/ThreeJsRenderer.hx
@@ -4,13 +4,13 @@ package src.renderer;
 
 import js.Browser;
 import js.html.DivElement;
-import js.three.Face3;
 import js.three.Geometry;
 import js.three.Mesh;
 import js.three.MeshBasicMaterial;
-import js.three.PerspectiveCamera;
+import js.three.Object3D;
+import js.three.OrthographicCamera;
+import js.three.PlaneGeometry;
 import js.three.Scene;
-import js.three.Vector3;
 import js.three.WebGLRenderer;
 import src.shape.Shape;
 import src.shape.ShapeTypes;
@@ -21,38 +21,25 @@ import src.shape.ShapeTypes;
  */
 @:keep
 class ThreeJsRenderer {
-	var camera:PerspectiveCamera;
+	var camera:OrthographicCamera;
 	var renderer:WebGLRenderer = new WebGLRenderer();
 	var scene:Scene = new Scene();
+	var meshes:Object3D = new Object3D();
 	
 	public function new() {
 		var container:DivElement = cast Browser.window.document.getElementById("basic_logo_container"); // TODO make this less broken
 		
-		renderer.setClearColor(0x000000, 1); // TODO
+		renderer.setClearColor(0xFFFFFF, 1); // TODO
 		renderer.setSize(800, 600); // TODO make this less broken
 		
 		container.appendChild(renderer.domElement);
 		
-		camera = new PerspectiveCamera(45, 800 / 600, 1, 100);
-		camera.position.set(0, 0, 10);
+		camera = new OrthographicCamera(0, 800, 0, 600, 0, 1000);
+		camera.position.set(200, 100, 200);
+		camera.position.set(0, 0, 200);
 		camera.lookAt(scene.position);
-		
 		scene.add(camera);
-		
-		// TODO some test code to get something rendering
-		var triangleGeometry = new Geometry();
-		triangleGeometry.vertices.push(new Vector3(0.0, 1.0, 0.0));
-		triangleGeometry.vertices.push(new Vector3(-1.0, -1.0, 0.0));
-		triangleGeometry.vertices.push(new Vector3(1.0, -1.0, 0.0));
-		triangleGeometry.faces.push(new Face3(0, 1, 2));
-		
-		var triangleMaterial = new MeshBasicMaterial({color:0xFFFFFF});
-		
-		var triangleMesh = new Mesh(triangleGeometry, triangleMaterial);
-		
-		triangleMesh.position.set( -1.5, 0.0, 4.0);
-		
-		scene.add(triangleMesh);
+		scene.add(meshes);
 	}
 	
 	public function render(shapes:Array<Shape>) {
@@ -82,14 +69,19 @@ class ThreeJsRenderer {
 		}
 		
 		renderer.render(scene, camera);
+		
+		clearScene();
 	}
 	
 	private inline function drawRectangle(s:Shape) {
-
+		var geometry = new PlaneGeometry(s.data[2] - s.data[0], s.data[3] - s.data[1]);
+		var mesh = makeMesh(geometry, s.color);
+		mesh.position.set(s.data[0] + ((s.data[2] - s.data[0]) / 2), s.data[1] + ((s.data[3] - s.data[1]) / 2), 4.0);
+		meshes.add(mesh);
 	}
 	
 	private inline function drawRotatedRectangle(s:Shape) {
-
+		
 	}
 	
 	private inline function drawTriangle(s:Shape) {
@@ -118,6 +110,33 @@ class ThreeJsRenderer {
 	
 	private inline function drawPolyline(s:Shape) {
 
+	}
+	
+	private inline function clearScene() {
+		for (mesh in meshes.children) {
+			var m:Mesh = cast mesh;
+			var g:Geometry = cast m.geometry;
+			g.dispose();
+			
+			m.material.dispose();
+		}
+		
+		meshes = new Object3D();
+	}
+	
+	// Creates a material from an RGBA8888 color value
+	private inline function makeMaterial(color:Int):MeshBasicMaterial {
+		var rgb:Int = color >> 8;
+		
+		if (color & 0xFF == 0xFF) {
+			return new MeshBasicMaterial({color:rgb, side:js.Three.DoubleSide});
+		}
+		var opacity:Float = (color & 0xFF) / 255.0;
+		return new MeshBasicMaterial({color:rgb, transparent:true, opacity:opacity, side:js.Three.DoubleSide});
+	}
+	
+	private inline function makeMesh(geometry:Geometry, color:Int):Mesh {
+		return new Mesh(geometry, makeMaterial(color));
 	}
 }
 
